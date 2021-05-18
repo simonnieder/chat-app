deb50c274501c2d1267a975420c98403feb5d773
simonnnnnnnnnnn <niederwolfsgruber16@gmail.com>
2021-05-18 19:59:57 +0200

    input error

---
+++ client/src/components/InputError.js
@@ -0,0 +1,11 @@
+const InputError = ({msg}) => {
+    return (
+            <div className="font-inter font-normal text-red-600 bg-red-100 text-md p-2 w-full rounded-lg flex items-center space-x-3">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <p>{msg}</p>
+            </div>
+    )
+}
+export default InputError
